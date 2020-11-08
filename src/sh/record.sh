df5a04238441b2a23da524a718d17412a77b3ba8
KthrnG <kgedrat@hfk-bremen.de>
2020-11-08 12:30:10 +0100

    Update record.sh

--- src/sh/record.sh
+++ src/sh/record.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-while [ true ]; do
-  DATE=`date`
-  arecord -D hw:1 -d 10 -f S16_LE -r 48000 "$DATE.wav"
+beendet=false
+
+trap beenden SIGINT
+trap beenden SIGTERM
+
+function beenden(){
+	beendet=true
+	echo "Stoppe Aufnahme..."
+}
+
+while [ "$beendet" = false ]; do
+	date=$(date +%s)
+	arecord -D hw:1 -d 10 -f cd -r 48000 -c 1 | lame -v - "$TSCHILP_AUDIOVERZEICHNIS/$date.tmp"
+	mv "$TSCHILP_AUDIOVERZEICHNIS/$date.tmp" "$TSCHILP_AUDIOVERZEICHNIS/$date.mp3"
 done
