6c1aee37dac4e83b920ab0261abbd0f70480ce23
kthrnG <kgedrat@hfk-bremen.de>
2021-02-27 14:37:38 +0100

    Formatierung & beenden verbessern

--- src/analyse.sh
+++ src/analyse.sh
@@ -2,25 +2,27 @@
 
 # Aufgenommene Audiodateien mit BIRD.net analysieren.
 
-beendet=false
-
 trap beenden SIGINT SIGTERM
 
-function beenden(){
-	echo "Stoppe Analyse ..."
+function beenden() {
+  echo "Stoppe Analyse ..."
+  exit
 }
 
-cd $TSCHILP_BIRDNETVERZEICHNIS
+cd $TSCHILP_BIRDNETVERZEICHNIS || exit
 
 while [ true ]; do
-	if [ -z "$(ls $TSCHILP_AUDIOVERZEICHNIS)" ]; then
-		sleep 60 & wait $!
-		continue
-	fi
-	for datei in $TSCHILP_AUDIOVERZEICHNIS/*.wav; do
-		timestamp=`basename -s .wav $datei`
-		python3 analyze.py --i "$datei" --o "$TSCHILP_ANALYSEVERZEICHNIS/$timestamp.csv" --min_conf 0.5 --week `date +%V` --lat 53.073 --lon 8.806
-	       	mv $datei $TSCHILP_AUDIOARCHIV/
-	done
-	sleep 60 & wait $!
+  if [ -z "$(ls $TSCHILP_AUDIOVERZEICHNIS)" ]; then
+    sleep 60 &
+    wait $!
+    continue
+  fi
+  for datei in $TSCHILP_AUDIOVERZEICHNIS/*.wav; do
+    timestamp=$(basename -s .wav $datei)
+    week=$(date +%V)
+    python3 analyze.py --i "$datei" --o $TSCHILP_ANALYSEVERZEICHNIS/$timestamp.csv --min_conf 0.5 --week $week --lat 53.073 --lon 8.806
+    mv $datei $TSCHILP_AUDIOARCHIV/
+  done
+  sleep 60 &
+  wait $!
 done
--- src/notify.sh
+++ src/notify.sh
@@ -2,7 +2,14 @@
 
 # Wertet das Ergebnis der Analyse aus und versendet eine E-Mail bei neu beobachteten Vögeln.
 
-sqlite3 "$TSCHILP_DATENBANKVERZEICHNIS/beobachtungen.sqlite" << 'END_SQL'
+trap beenden SIGINT SIGTERM
+
+function beenden() {
+  echo "Stoppe Analyse ..."
+  exit
+}
+
+sqlite3 "$TSCHILP_DATENBANKVERZEICHNIS/beobachtungen.sqlite" <<'END_SQL'
 CREATE TABLE IF NOT EXISTS voegel (
   id TEXT PRIMARY KEY,
   name TEXT,
@@ -13,26 +20,28 @@ CREATE TABLE IF NOT EXISTS voegel (
 END_SQL
 
 while [ true ]; do
-	if [ -z "$(ls $TSCHILP_ANALYSEVERZEICHNIS)" ]; then
-		sleep 60
-		continue
-	fi
-	for datei in $TSCHILP_ANALYSEVERZEICHNIS/*.csv; do
-		while IFS=$';' read -r start end scientific_name common_name confidence; do
-			timestamp=`basename -s .csv $datei`
-			anzahl=`sqlite3 "$TSCHILP_DATENBANKVERZEICHNIS/beobachtungen.sqlite" "SELECT anzahl_beobachtungen FROM voegel WHERE id='$scientific_name';"`
-			if [ -z "$anzahl" ]; then
-				echo "Neuer Vogel: $common_name"
-				sqlite3 "$TSCHILP_DATENBANKVERZEICHNIS/beobachtungen.sqlite" "INSERT INTO voegel VALUES ('$scientific_name','$common_name',1,'$timestamp','$confidence')"
-				mail -s "Neuer Vogel: $common_name" $TSCHILP_EMAIL_EMPFAENGER -aFrom:Tschilp\<$TSCHILP_EMAIL_ABSENDER\> -A $TSCHILP_AUDIOARCHIV/$timestamp.wav  << 'END_MAIL'
+  if [ -z "$(ls $TSCHILP_ANALYSEVERZEICHNIS)" ]; then
+    sleep 60 &
+    wait $!
+    continue
+  fi
+  for datei in $TSCHILP_ANALYSEVERZEICHNIS/*.csv; do
+    while IFS=$';' read -r start end scientific_name common_name confidence; do
+      timestamp=$(basename -s .csv $datei)
+      anzahl=$(sqlite3 "$TSCHILP_DATENBANKVERZEICHNIS/beobachtungen.sqlite" "SELECT anzahl_beobachtungen FROM voegel WHERE id='$scientific_name';")
+      if [ -z "$anzahl" ]; then
+        echo "Neuer Vogel: $common_name"
+        sqlite3 "$TSCHILP_DATENBANKVERZEICHNIS/beobachtungen.sqlite" "INSERT INTO voegel VALUES ('$scientific_name','$common_name',1,'$timestamp','$confidence')"
+        mail -s "Neuer Vogel: $common_name" $TSCHILP_EMAIL_EMPFAENGER -aFrom:Tschilp\<$TSCHILP_EMAIL_ABSENDER\> -A $TSCHILP_AUDIOARCHIV/$timestamp.wav <<'END_MAIL'
 Ein neuer Vogel kam vorbei! https://de.wikipedia.org/wiki/$scientific_name
 END_MAIL
-			else
-				echo "Alter Vogel: $common_name ($anzahl)"
-				sqlite3 "$TSCHILP_DATENBANKVERZEICHNIS/beobachtungen.sqlite" "UPDATE voegel SET anzahl_beobachtungen = anzahl_beobachtungen + 1, letzte_beobachtung = '$timestamp', confidence = '$confidence' WHERE id='$scientific_name';"
-			fi
-		done < <(tail -n +2 $datei)
-		mv $datei $TSCHILP_ANALYSEARCHIV/
-	done
-	sleep 60
+      else
+        echo "Alter Vogel: $common_name ($anzahl)"
+        sqlite3 "$TSCHILP_DATENBANKVERZEICHNIS/beobachtungen.sqlite" "UPDATE voegel SET anzahl_beobachtungen = anzahl_beobachtungen + 1, letzte_beobachtung = '$timestamp', confidence = '$confidence' WHERE id='$scientific_name';"
+      fi
+    done < <(tail -n +2 $datei)
+    mv $datei $TSCHILP_ANALYSEARCHIV/
+  done
+  sleep 60 &
+  wait $!
 done
