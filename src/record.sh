4c0afbcf355d23a12297dc9c9fc34a8f8cfcbe70
kthrnG <kgedrat@hfk-bremen.de>
2021-02-27 14:41:27 +0100

    Formatierung & beenden verbessern

--- src/record.sh
+++ src/record.sh
@@ -2,17 +2,15 @@
 
 # Den Ton mit dem angeschlossenen Mikrofon kontinuierlich aufnehmen und in Dateien von 10 Sekunden Länge speichern.
 
-beendet=false
-
 trap beenden SIGINT SIGTERM
 
-function beenden(){
-	beendet=true
-	echo "Stoppe Aufnahme ..."
+function beenden() {
+  echo "Stoppe Aufnahme ..."
+  exit
 }
 
-while [ "$beendet" = false ]; do
-	date=$(date +%s)
-	arecord -D hw:1 -d 10 -f cd -r 48000 -c 1 "$TSCHILP_AUDIOVERZEICHNIS/$date.tmp"
-	mv "$TSCHILP_AUDIOVERZEICHNIS/$date.tmp" "$TSCHILP_AUDIOVERZEICHNIS/$date.wav"
+while [ true ]; do
+  date=$(date +%s)
+  arecord -D hw:1 -d 10 -f cd -r 48000 -c 1 "$TSCHILP_AUDIOVERZEICHNIS/$date.tmp"
+  mv "$TSCHILP_AUDIOVERZEICHNIS/$date.tmp" "$TSCHILP_AUDIOVERZEICHNIS/$date.wav"
 done
--- src/transfer.sh
+++ src/transfer.sh
@@ -2,27 +2,26 @@
 
 # Alle Audiodateien in dem Tschilp Audioverzeichnis per ssh auf einen Server kopieren und anschließend löschen.
 
-beendet=false
-
 trap beenden SIGINT SIGTERM
 
-function beenden(){
-	beendet=true
-	echo "Stoppe Transfer ..."
+function beenden() {
+  echo "Stoppe Transfer ..."
+  exit
 }
 
-function kopierenUndLoeschen(){
-	for audiodatei in "$TSCHILP_AUDIOVERZEICHNIS/*.wav" ; do
-		scp $audiodatei $TSCHILP_USER@$TSCHILP_SERVER:$TSCHILP_ZIELVERZEICHNIS
-		if [ "$?" -eq 0 ]; then
-			rm $audiodatei
-		fi
-	done
+function kopierenUndLoeschen() {
+  for audiodatei in $TSCHILP_AUDIOVERZEICHNIS/*.wav; do
+    scp $audiodatei $TSCHILP_USER@$TSCHILP_SERVER:$TSCHILP_ZIELVERZEICHNIS
+    if [ "$?" -eq 0 ]; then
+      rm $audiodatei
+    fi
+  done
 }
 
-while [ "$beendet" = false ]; do
-	if [ -n "$(ls -A $TSCHILP_AUDIOVERZEICHNIS)" ]; then
-		kopierenUndLoeschen
-	fi
-	sleep 60s
+while [ true ]; do
+  if [ -n "$(ls -A $TSCHILP_AUDIOVERZEICHNIS)" ]; then
+    kopierenUndLoeschen
+  fi
+  sleep 60 &
+  wait $!
 done
