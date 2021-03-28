7572f1eadac14d81d341163168055ca0cda92976
kthrnG <kgedrat@hfk-bremen.de>
2021-03-28 16:28:28 +0200

    Vogeldatenbank anlegen

--- db/create_db.sh
+++ db/create_db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function datenbank_anlegen() {
-  sqlite3 voegel.sqlite "CREATE TABLE IF NOT EXISTS voegel(scientific_name TEXT PRIMARY KEY, name_en TEXT, name_de TEXT, avibase_url TEXT, wikipedia_url TEXT,img TEXT);"
+  sqlite3 tschilp.sqlite "CREATE TABLE IF NOT EXISTS voegel(scientific_name TEXT PRIMARY KEY, name_en TEXT, name_de TEXT, avibase_url TEXT, wikipedia_url TEXT,img TEXT);"
 }
 
 function datenbank_fuellen() {
@@ -13,7 +13,7 @@ function datenbank_fuellen() {
     avibase_url=$(echo $vogel | jq '.avibase_url')
     wikipedia_url=$(echo $vogel | jq '.wikipedia_url')
     img=$(echo $vogel | jq '.img')
-    sqlite3 voegel.sqlite "INSERT INTO voegel VALUES($scientific_name,$name_en,$name_de,$avibase_url,$wikipedia_url,$img);"
+    sqlite3 tschilp.sqlite "INSERT INTO voegel VALUES($scientific_name,$name_en,$name_de,$avibase_url,$wikipedia_url,$img);"
   done
 }
 
