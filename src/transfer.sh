35d65e960b0a0e78c565dd5aeee59a32c032ebf2
kthrnG <kgedrat@hfk-bremen.de>
2021-04-03 14:23:25 +0200

    Ausgabe verbessern

--- src/transfer.sh
+++ src/transfer.sh
@@ -13,13 +13,14 @@ function kopierenUndLoeschen() {
   for audiodatei in $TSCHILP_AUDIOVERZEICHNIS/*.wav; do
     scp $audiodatei $TSCHILP_USER@$TSCHILP_SERVER:$TSCHILP_ZIELVERZEICHNIS
     if [ "$?" -eq 0 ]; then
+      echo "$audiodatei übertragen"
       rm $audiodatei
     fi
   done
 }
 
 while true; do
-  if [ -n "$(ls $TSCHILP_AUDIOVERZEICHNIS)" ]; then
+  if [ -n "$(ls $TSCHILP_AUDIOVERZEICHNIS)" ] && ping -c 1 $TSCHILP_SERVER; then
     kopierenUndLoeschen
   fi
   sleep 3 &
